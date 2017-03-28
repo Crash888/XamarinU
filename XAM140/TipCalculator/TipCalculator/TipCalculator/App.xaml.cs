3131b651afc65dc86128324988758acfae0dcd0a
dd <ddresearch@shaw.ca>
2017-03-28 11:30:40 -0700

    XAM140 update

---
+++ XAM140/TipCalculator/TipCalculator/TipCalculator/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+			MainPage = new NavigationPage(new StandardTipPage());
+		}
+	}
+}
