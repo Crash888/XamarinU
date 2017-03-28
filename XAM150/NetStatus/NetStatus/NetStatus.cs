0f5bc2851ef52af3ec0f332a5bab279a3e141dfd
dd <ddresearch@shaw.ca>
2017-03-28 15:03:44 -0700

    XAM150

---
+++ XAM150/NetStatus/NetStatus/NetStatus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+
+namespace NetStatus
+{
+	public class App : Application
+	{
+		public App()
+		{
+			MainPage = CrossConnectivity.Current.IsConnected ? (Page)new NetworkViewPage() : new NoNetworkPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+			base.OnStart();
+			CrossConnectivity.Current.ConnectivityChanged += HandleConnectivityChanged;
+
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+
+		void HandleConnectivityChanged(object sender, ConnectivityChangedEventArgs e) 
+		{
+			Type currentPage = this.MainPage.GetType();
+
+			if (e.IsConnected && currentPage != typeof(NetworkViewPage))
+				this.MainPage = new NetworkViewPage();
+			else if (!e.IsConnected && currentPage != typeof(NoNetworkPage))
+				this.MainPage = new NoNetworkPage();
+		}
+	}
+}
---
+++ XAM150/NetStatus/NetStatus/NetworkViewPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+	public partial class NetworkViewPage : ContentPage
+	{
+		Label ConnectionDetails;
+
+		public NetworkViewPage()
+		{
+			InitializeComponent();
+
+			ConnectionDetails = new Label()
+			{
+				Text = "Connection Type",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+			};
+
+			Content = ConnectionDetails;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			ConnectionDetails.Text = CrossConnectivity.Current.ConnectionTypes.First().ToString();
+
+			if (CrossConnectivity.Current != null)
+				CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+
+			if (CrossConnectivity.Current != null)
+				CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+
+		}
+		private void UpdateNetworkInfo(object sender, ConnectivityChangedEventArgs e)
+		{
+			if (CrossConnectivity.Current != null && CrossConnectivity.Current.ConnectionTypes != null)
+			{
+				var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+				ConnectionDetails.Text = connectionType.ToString();
+			}
+		}
+	}
+}
